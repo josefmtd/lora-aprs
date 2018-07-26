ebe0f50215023435091b01f4d01c6ca385c348be
Josef Matondang <josstemat@gmail.com>
2018-07-26 19:00:30 +0700

    Added transmit capability

--- lora-kiss/lora-kiss.ino
+++ lora-kiss/lora-kiss.ino
@@ -8,8 +8,18 @@
 
 #define maxPacketSize 255
 
-void putSerial(const uint8_t *const send_buffer, const uint16_t send_buffer_size) {
-  Serial.write(send_buffer, send_buffer_size);
+#define kissNoCrc   0x00
+#define kissFlexnet 0x20
+#define kissSmack   0x80
+
+void putSerial(const uint8_t *const txBuffer, const uint16_t txBufferSize) {
+  Serial.write(txBuffer, txBufferSize);
+}
+
+void putRadio(const uint8_t *const txBuffer, const uint16_t txBufferSize) {
+  LoRa.beginPacket();
+  LoRa.write(txBuffer, txBufferSize);
+  LoRa.endPacket();
 }
 
 void putByte(uint8_t *const out, uint16_t *const offset, const uint8_t buffer) {
@@ -105,15 +115,101 @@ void debug(const char *const debug_string) {
 //  putSerial(&fend, 1);
 //}
 
+bool getSerial(uint8_t *const rxBuffer, const uint16_t rxBufferSize, const unsigned long int timeOut) {
+  for (uint16_t i = 0; i < rxBufferSize; i++) {
+    while(!Serial.available()) {
+      if (millis() >= timeOut) {
+        return false;
+      }
+    }
+    rxBuffer[i] = Serial.read();
+  }
+  return true;
+}
+
+void processSerial() {
+  bool first = true;
+  bool frameValid = false;
+  bool escape = false;
+  uint8_t bufferSmall[maxPacketSize];
+  
+  uint16_t offset = 0;
+
+  const unsigned long int end = millis() + 5000;
+
+  while(millis() < end && frameValid == false && offset < maxPacketSize) {
+    uint8_t buffer = 0;
+
+    if (!getSerial(&buffer, 1, end)) {
+      debug("SERIAL TIMEOUT");
+      break;
+    }
+
+    switch(buffer) {
+      case FEND:
+        if (first) {
+          first = false;
+        }
+        else {
+          frameValid = true;
+        }
+        break;
+      case FESC:
+        escape = true;
+      default:
+        if (escape) {
+          if (buffer == TFEND) {
+            bufferSmall[offset++] = FEND;
+          }
+          else if (buffer == TFESC) {
+            bufferSmall[offset++] = FESC;
+          }
+          else {
+            debug("ERROR ESCAPE");
+          }
+
+          escape = false;
+        }
+        else {
+          bufferSmall[offset++] = buffer;
+        }
+    }
+  }
+
+  if (frameValid) {
+    if (offset > 1) {
+      switch(bufferSmall[0]) {
+        case kissNoCrc:
+          // debug("Should transmit");
+          putRadio(&bufferSmall[0], offset);
+          break;
+        case kissFlexnet:
+          debug("CANNOT ACCEPT FLEXNET");
+          break;
+        case kissSmack:
+          debug("CANNOT ACCEPT SMACK");
+        default:
+          debug("FRAME UNKNOWN");
+      }
+    }
+    else {
+      debug("TOO SMALL FOR A KISS FRAME");
+    }
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(38400);
   while (!Serial);
-  
+  //for LoRa32u4
+  LoRa.setPins(8, 4, 7);
   if (!LoRa.begin(434E6)) {
     debug("LoRa Init Failed");
   }
   else {
+    LoRa.enableCrc();
+    LoRa.setCodingRate4(9);
     LoRa.onReceive(onReceive);
     LoRa.receive();
     debug("LoRa Init Success");
@@ -122,6 +218,11 @@ void setup() {
 
 void loop() {
   // do nothing
+  uint16_t serialIn = Serial.available();
+  if (serialIn != 0) {
+    // debug("IT'S SOMETHING");
+    processSerial();
+  }
 }
 
 void onReceive(int PacketSize) {
@@ -142,4 +243,3 @@ void onReceive(int PacketSize) {
 
   putSerial(bufferBig, offsetBig);
 }
-
